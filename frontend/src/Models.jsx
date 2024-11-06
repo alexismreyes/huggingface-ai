b1d9022449710dff10b9d2355a5e48b569b097cd
alexismreyes <alexis_mreyes@hotmail.com>
2024-11-06 10:44:37 -0600

    Updated Models.jsx to be able to show background image in production

--- frontend/src/Models.jsx
+++ frontend/src/Models.jsx
@@ -1,13 +1,14 @@
 import ImageToText from './components/ImageToText';
 import TextGeneration from './components/TextGeneration';
 import Translate from './components/Translate';
+import bgImage from '../assets/img/bg-01.jpg';
 
 const Models = () => {
   return (
     <>
       <div
         className="container-login100"
-        style={{ backgroundImage: 'url(../assets/img/bg-01.jpg)' }}
+        style={{ backgroundImage: `url(${bgImage})` }}
       >
         <div className="wrap-login100 p-l-55 p-r-55 p-t-65">
           <form className="login100-form">
